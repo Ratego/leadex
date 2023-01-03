7147503b785577a58f4f1e5aab376a66f0166df7
Ratego <advanceddeveloper2020@gmail.com>
2023-01-03 13:07:19 +0300

    Updating server url

--- client/script.js
+++ client/script.js
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
   Loader (messageDiv);
 
   //fetching data from server - getting bot response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://leadex.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type':'application/json'
